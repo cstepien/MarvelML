4ab4b6e43f174d229dd357da047a2637f9cae0d9
cstepien <courtney.stepien@gmail.com>
2018-01-04 22:37:39 -0600

    Create R Project and  start scrape script
    Next steps: pull character list from the API

---
+++ ScrapeMarvel.R
@@ -0,0 +1,8 @@
+# Scrape Data from Marvel Universe Wiki
+
+library(rvest)
+library(dplyr)
+
+# First we need to scrape the list of characters in the wiki (or pull from API)
+
+# Then we can scrape data from their individual pages
